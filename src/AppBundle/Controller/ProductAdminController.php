17223bce2325d388bde3172264d180784d5d2e47
Ryan Weaver <ryan@thatsquality.com>
2015-09-18 10:24:32 -0400

    Removing all the new product stuff - we will add this BDD

--- src/AppBundle/Controller/ProductAdminController.php
+++ src/AppBundle/Controller/ProductAdminController.php
@@ -23,38 +23,4 @@ class ProductAdminController extends Controller
             'products' => $products
         ]);
     }
-
-    /**
-     * @Route("/admin/products/new", name="product_new")
-     * @Method("GET")
-     */
-    public function newAction()
-    {
-        sleep(1);
-
-        return $this->render('product/_product_new.html.twig');
-    }
-
-    /**
-     * @Route("/admin/products/new", name="product_new_handle")
-     * @Method("POST")
-     */
-    public function handleNewAction(Request $request)
-    {
-        // really quick (but dangerous) way to load my data into a Product
-
-        $product = new Product();
-        $product->setName($request->get('name'));
-        $product->setDescription($request->get('description'));
-        $product->setPrice($request->get('price'));
-
-        $product->setAuthor($this->getUser());
-        $this->getDoctrine()->getManager()->persist($product);
-        $this->getDoctrine()->getManager()->flush();
-
-
-        $this->addFlash('success', 'Product created FTW!');
-
-        return $this->redirect($this->generateUrl('product_list'));
-    }
 }
