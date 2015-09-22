ef9b13e056474755476ef9eb04cd26d0e8f28e8b
Ryan Weaver <ryan@thatsquality.com>
2015-09-22 12:55:21 -0400

    [tuts-apply-diff] Applying existing diff file for step mink-using-selenium2driver

--- mink.php
+++ mink.php
@@ -3,10 +3,12 @@
 require __DIR__.'/vendor/autoload.php';
 
 use Behat\Mink\Driver\GoutteDriver;
+use Behat\Mink\Driver\Selenium2Driver;
 use Behat\Mink\Session;
 
 // Important object #1
-$driver = new GoutteDriver();
+//$driver = new GoutteDriver();
+$driver = new Selenium2Driver();
 
 // Important object #2
 $session = new Session($driver);
@@ -14,7 +16,7 @@ $session = new Session($driver);
 $session->start();
 $session->visit('http://jurassicpark.wikia.com');
 
-echo "Status code: ". $session->getStatusCode() . "\n";
+//echo "Status code: ". $session->getStatusCode() . "\n";
 echo "Current URL: ". $session->getCurrentUrl() . "\n";
 
 // Important object #3 DocumentElement
@@ -36,3 +38,5 @@ echo "The link href is: ". $linkEl->getAttribute('href') . "\n";
 
 $linkEl->click();
 echo "Page URL after click: ". $session->getCurrentUrl() . "\n";
+
+$session->stop();
