d06e07f7f5c2e6179ac1dd265cf79fc517f43696
Ryan Weaver <ryan@thatsquality.com>
2015-09-22 13:30:46 -0400

    [tuts-apply-diff] Applying existing diff file for step javascript-breaking

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -143,6 +143,19 @@ class FeatureContext extends RawMinkContext implements Context, SnippetAccepting
     }
 
     /**
+     * Pauses the scenario until the user presses a key. Useful when debugging a scenario.
+     *
+     * @Then (I )break
+     */
+    public function iPutABreakpoint()
+    {
+        fwrite(STDOUT, "\033[s    \033[93m[Breakpoint] Press \033[1;93m[RETURN]\033[0;93m to continue...\033[0m");
+        while (fgets(STDIN, 1024) == '') {}
+        fwrite(STDOUT, "\033[u");
+        return;
+    }
+
+    /**
      * @return \Behat\Mink\Element\DocumentElement
      */
     private function getPage()
--- features/web/product_admin.feature
+++ features/web/product_admin.feature
@@ -23,6 +23,7 @@ Feature: Product admin panel
     And I am on "/admin/products"
     When I click "New Product"
     And I wait for the modal to load
+    And break
     And I fill in "Product Name" with "Veloci-chew toy"
     And I fill in "Price" with "20"
     And I fill in "Description" with "Have your velociraptor chew on this instead!"
