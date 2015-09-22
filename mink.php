346521d82458901e2c92d40919493574722c77e0
Ryan Weaver <ryan@thatsquality.com>
2015-09-22 12:55:19 -0400

    [tuts-apply-diff] Applying existing diff file for step mink-create-use-session

--- mink.php
+++ mink.php
@@ -3,6 +3,16 @@
 require __DIR__.'/vendor/autoload.php';
 
 use Behat\Mink\Driver\GoutteDriver;
+use Behat\Mink\Session;
 
 // Important object #1
 $driver = new GoutteDriver();
+
+// Important object #2
+$session = new Session($driver);
+
+$session->start();
+$session->visit('http://jurassicpark.wikia.com');
+
+echo "Status code: ". $session->getStatusCode() . "\n";
+echo "Current URL: ". $session->getCurrentUrl() . "\n";
